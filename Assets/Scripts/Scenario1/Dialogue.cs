4dd9ec774f46609de88f1c34ed5e9cd78dfeb8ea
Diwash Mainali <diwashm.189@gmail.com>
2024-02-18 10:05:41 +0545

    First Commit

---
+++ Assets/Scripts/Scenario1/Dialogue.cs
@@ -0,0 +1,14 @@
+﻿
+
+using UnityEngine;
+
+[System.Serializable]
+
+public class Dialogue
+/*  This class holds a dialogue in the form of 2-10 sentences.
+*/
+
+{    
+    [TextArea(2, 10)]
+    public string[] sentences;
+}
